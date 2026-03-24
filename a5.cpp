621ea1e4cb6e9e29dedff101bf212cc0bc279967
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2026-03-24 22:21:18 +0530

    Add files via upload

---
+++ a5.cpp
@@ -0,0 +1,18 @@
+// Missing in Array
+#include<iostream>
+using namespace std;
+
+class Solution {
+  public:
+    int missingNum(vector<int>& arr) {
+        long long n = arr.size() + 1;
+        long long total = n * (n + 1) / 2;
+        
+        long long sum = 0;
+        for(int i = 0; i < arr.size(); i++){
+            sum += arr[i];
+        }
+        
+        return (int)(total - sum);
+    }
+};
