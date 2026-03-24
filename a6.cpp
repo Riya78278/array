255dd27a4525dfd24d25494a5c309832dd5d5083
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2026-03-24 22:21:30 +0530

    Add files via upload

---
+++ a6.cpp
@@ -0,0 +1,28 @@
+//Max Consecutive Bit
+#include<iostream>
+using namespace std;
+
+class Solution {
+  public:
+    int maxConsecBits(vector<int> &arr) {
+        // code here
+        if(arr.size()==0){
+            return 0;
+        }
+        int maxi=1;
+        int count=1;
+        int nums=arr[0];
+        for(int i=1;i<arr.size();i++){
+            if(nums==arr[i]){
+                count++;
+                maxi=max(maxi,count);
+            }
+            else{
+                count=1;
+                nums=arr[i];
+            }
+        }
+        return maxi;
+        
+    }
+};
