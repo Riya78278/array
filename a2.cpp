878d584114e1239f68db642a8ac36bca9f985b6a
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2026-03-24 22:20:41 +0530

    Add files via upload

---
+++ a2.cpp
@@ -0,0 +1,19 @@
+// Rotate Array
+#include<iostream>
+using namespace std;
+
+class Solution {
+  public:
+    void rotateArr(vector<int>& arr, int d) {
+        // code here
+        int n=arr.size();
+        
+        vector<int>temp(n);
+        for(int i=0;i<n;i++){
+            temp[i]=arr[(i+d)%n];
+        }
+        arr=temp;
+        
+        
+    }
+};
