c15c5c63cf9aa1fbb7f9408a9920d5c0e36de3d0
RIYA MONDAL <142240539+Riya78278@users.noreply.github.com>
2026-03-24 23:25:18 +0530

    Add files via upload

---
+++ a9.cpp
@@ -0,0 +1,24 @@
+//Two Sum - Pair with Given Sum
+#include<iostream>
+#include<set>
+
+using namespace std;
+
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+        // code here
+        if(arr.size()<2){
+            return false;
+        }
+        set<int>st;
+        for(int i=0;i<arr.size();i++){
+            int ans=target-arr[i];
+            if(st.find(ans)!=st.end()){
+                return true;
+            }
+            st.insert(arr[i]);
+        }
+        return false;
+    }
+};
